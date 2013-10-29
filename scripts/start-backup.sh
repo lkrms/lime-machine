61c7bbb8ec6cf387b32e4dc9cf8b4e2954f9caa2
Luke Arms <code@lukearms.com.au>
2013-10-29 21:15:35 +1100

    License header added.

--- scripts/start-backup.sh
+++ scripts/start-backup.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# lime-machine: Linux backup software inspired by Time Machine on OS X.
+# Copyright (c) 2013 Luke Arms
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd)
 BACKUP_ROOT=$(cd "$SCRIPT_DIR/.."; pwd)
 CONFIG_DIR=$BACKUP_ROOT/config
