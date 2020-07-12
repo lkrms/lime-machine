92db574cc64d4c4de1f82b14c2be0a84ad199b56
Luke Arms <lina@x390.localdomain>
2020-07-13 00:43:13 +1000

    Delete log files older than 90 days

--- scripts/common.sh
+++ scripts/common.sh
@@ -9,7 +9,10 @@ fi
 
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 
-BACKUP_ROOT="$(cd "$SCRIPT_DIR/.."; pwd -P)"
+BACKUP_ROOT="$(
+    cd "$SCRIPT_DIR/.."
+    pwd -P
+)"
 CONFIG_DIR="$BACKUP_ROOT/config"
 SCRIPT_NAME="$(basename "$0")"
 BASH_MAJOR_VERSION="${BASH_VERSINFO[0]}"
@@ -25,11 +28,23 @@ SEQUENTIAL_OPERATIONS=0
 
 . "$CONFIG_DIR/settings"
 
-mkdir -p "$(dirname "$LOG_FILE")" || { echo "Error: $(dirname "$LOG_FILE") doesn't exist."; exit 1; }
-touch "$LOG_FILE" || { echo "Error: unable to open $LOG_FILE for writing."; exit 1; }
+mkdir -p "$(dirname "$LOG_FILE")" || {
+    echo "Error: $(dirname "$LOG_FILE") doesn't exist."
+    exit 1
+}
+touch "$LOG_FILE" || {
+    echo "Error: unable to open $LOG_FILE for writing."
+    exit 1
+}
 
-mkdir -p "$RUN_DIR" || { echo "Error: $RUN_DIR doesn't exist."; exit 1; }
-[ -w "$RUN_DIR" ] || { echo "Error: unable to write to $RUN_DIR."; exit 1; }
+mkdir -p "$RUN_DIR" || {
+    echo "Error: $RUN_DIR doesn't exist."
+    exit 1
+}
+[ -w "$RUN_DIR" ] || {
+    echo "Error: unable to write to $RUN_DIR."
+    exit 1
+}
 
 if [ ! -z "$PROXY_SERVICE" ]; then
 
@@ -40,7 +55,7 @@ fi
 
 ERROR_LOG=""
 
-function log_error {
+function log_error() {
 
     # don't output to stderr unless we're on a tty
     tty -s && echo -e "$@" >&2
@@ -48,12 +63,12 @@ function log_error {
     local THIS_ERROR
     THIS_ERROR="$(log_time) $@"
 
-    echo -e "$THIS_ERROR" >> "$LOG_FILE"
+    echo -e "$THIS_ERROR" >>"$LOG_FILE"
     ERROR_LOG="${ERROR_LOG}${THIS_ERROR}\n"
 
 }
 
-function send_error_log {
+function send_error_log() {
 
     if [ ! -z "$ERROR_LOG" ]; then
 
@@ -70,52 +85,52 @@ function send_error_log {
 
 trap "send_error_log" EXIT
 
-function log_message {
+function log_message() {
 
     # don't output to stdout unless we're on a tty
     tty -s && echo -e "$@"
 
-    echo -e "$(log_time) $@" >> "$LOG_FILE"
+    echo -e "$(log_time) $@" >>"$LOG_FILE"
 
 }
 
-function log_source {
+function log_source() {
 
-    echo -e "$(log_time) $@" >> "$SOURCE_LOG_FILE"
+    echo -e "$(log_time) $@" >>"$SOURCE_LOG_FILE"
 
 }
 
-function escape_for_sed {
+function escape_for_sed() {
 
     echo "$@" | sed 's/[\/&]/\\&/g'
 
 }
 
-function snapshot2date {
+function snapshot2date() {
 
     echo "${1:0:10} ${1:11:2}:${1:13:2}:${1:15:2}"
 
 }
 
-function date2timestamp {
+function date2timestamp() {
 
     date -d "$1" "+%s"
 
 }
 
-function now2timestamp {
+function now2timestamp() {
 
     date "+%s"
 
 }
 
-function log_time {
+function log_time() {
 
     date "+%b %d %T"
 
 }
 
-function dump_args {
+function dump_args() {
 
     echo -e "Argument(s) passed to $1:\n"
 
@@ -125,7 +140,7 @@ function dump_args {
 
     for ARG in "$@"; do
 
-        (( ARG_NO++ ))
+        ((ARG_NO++))
         echo "$ARG_NO: $ARG"
 
     done
@@ -134,13 +149,13 @@ function dump_args {
 
 }
 
-function get_targets {
+function get_targets() {
 
     find "$BACKUP_ROOT/targets" -type f ! -iname '.*' ! -iname '*.inactive' ! -iname 'README.*' -print0 | sort -z
 
 }
 
-function close_targets {
+function close_targets() {
 
     # allow time for any pending file locks to be released
     sleep 30
@@ -161,6 +176,9 @@ function close_targets {
         # compress log files more than 2 days old
         find "$TARGET_MOUNT_POINT/logs" -type f -name '*.log' -mtime +2 -exec gzip '{}' \;
 
+        # delete log files older than 90 days
+        find "$TARGET_MOUNT_POINT/logs" -type f \( -name '*.log' -o -name '*.log.gz' \) -mtime +90 -delete
+
         if [ "$TARGET_UNMOUNT" -eq "1" ]; then
 
             if ! umount "$TARGET_MOUNT_POINT"; then
@@ -175,7 +193,7 @@ function close_targets {
 
 }
 
-function check_target {
+function check_target() {
 
     if [ -z "$TARGET_MOUNT_POINT" ]; then
 
@@ -215,15 +233,14 @@ function check_target {
 
 }
 
-function get_sources {
+function get_sources() {
 
     find "$BACKUP_ROOT/active-sources" \( -type f -o -type l \) ! -iname '.*' ! -iname 'README.*' -print0 | sort -z
 
 }
 
-function get_used_space {
+function get_used_space() {
 
     df "$TARGET_MOUNT_POINT" | tail -1 | awk '{print $5}' | sed 's/[^0-9]//g'
 
 }
-
